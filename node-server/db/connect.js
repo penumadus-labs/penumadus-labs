69068bf12621a07ddf0986a346d84d4cab2638a2
AB <abramabradley@gmail.com>
2020-04-03 19:08:09 -0400

    server update

--- node-server/db/connect.js
+++ node-server/db/connect.js
@@ -5,10 +5,14 @@ const development = process.env.NODE_ENV === 'development'
 
 const url = development
   ? `mongodb://localhost/admin`
-  : `mongodb://caro:Matthew85!!@localhost/test`
+  : `mongodb://caro:Matthew85!!@localhost/admin`
+
+console.log(development)
 
 module.exports = async () => {
   if (development) await tunnel()
+
+  console.log('trying connection')
 
   const client = await connect(url, {
     useNewUrlParser: true,
