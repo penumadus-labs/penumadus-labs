51bac03e15aca66659483b7aa77ac7c26e9188d3
AB <abramabradley@gmail.com>
2020-04-05 11:11:34 -0400

    server update

--- node-server/server.js
+++ node-server/server.js
@@ -6,7 +6,7 @@ const createWebsocketServer = require('./sockets/sockets')
 const { join, resolve } = require('path')
 
 const development = process.env.NODE_ENV === 'development'
-const port = 8000
+const port = 3000
 
 const app = express()
 
