cb39a636518ccd2c18065854cb03defd01ecfc24
AB <abramabradley@gmail.com>
2020-04-05 03:23:29 -0400

    frontend updates

--- frontend/gatsby-ssr.js
+++ frontend/gatsby-ssr.js
@@ -1,6 +1,1 @@
-import React from 'react'
-import ThemeProvider from './src/context/theme/theme-provider'
-
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>{element}</ThemeProvider>
-)
+export { wrapPageElement } from './wrap-page-element'
