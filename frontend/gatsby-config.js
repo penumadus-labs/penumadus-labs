bde84df18fa61e68f827a9ab80c4986b1b1195fc
Ubuntu <ubuntu@ip-172-31-42-56.us-east-2.compute.internal>
2020-04-06 15:56:58 +0000

    frontend updates

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -3,8 +3,7 @@ const activeEnv =
   process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
 
 console.log(`Using environment config: ${activeEnv}`)
-
-require('dotenv').config({ path: `.env${activeEnv}` })
+require('dotenv').config({ path: `.env.${activeEnv}` })
 
 module.exports = {
   siteMetadata: {
