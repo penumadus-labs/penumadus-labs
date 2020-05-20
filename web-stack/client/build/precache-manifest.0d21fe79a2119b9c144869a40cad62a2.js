0945a974e177d64110e65099288cb82cee2fd6b0
AB <abramabradley@gmail.com>
2020-05-20 14:50:55 -0400

    updates

---
+++ web-stack/client/build/precache-manifest.0d21fe79a2119b9c144869a40cad62a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d5ae70516ab0d72c365769efcb81dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a00221f378e994a9ecc",
+    "url": "/static/css/main.130cc324.chunk.css"
+  },
+  {
+    "revision": "0d599be45ab744eb320b",
+    "url": "/static/js/2.9864f26c.chunk.js"
+  },
+  {
+    "revision": "2b9e18be1c3d0f47c8bbbf1d59e3682c",
+    "url": "/static/js/2.9864f26c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a00221f378e994a9ecc",
+    "url": "/static/js/main.854c7d36.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
