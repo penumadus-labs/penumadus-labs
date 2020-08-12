80fc2f37cf996ad899258d8d98a8afc652fff161
Ubuntu <ubuntu@ip-172-31-42-56.us-east-2.compute.internal>
2020-08-12 04:58:49 +0000

    changes

--- web-stack/server/scripts/test.js
+++ web-stack/server/scripts/test.js
@@ -4,20 +4,10 @@ const field = 'standardData.'
 const value = 'pressure'
 
 client.wrap(async () => {
-  const res = await client.devices.findOne(
-    {
-      id: 'unit_3',
-      // $and: [
-      //   { [field]: { $gte: 1590553627.174376 } },
-      //   { [field]: { $lte: 1590567962.530352 } },
-      // ],
-    },
-    {
-      projection: {
-        _id: 0,
-        'standardData.pressure': 1,
-        'standardData.time': { $gte: {} },
-      },
-    }
-  )
+  const res = await client.devices.aggregate([
+    {$match: {id: 'unit_3'}},
+    { $unwind: '$standardData' },
+  ]).toArray()
+
+  console.log(res)
 })
