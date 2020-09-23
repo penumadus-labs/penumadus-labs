c34eebb276e5bd124d4a0ef57fb9df9bc5c7ac12
Ubuntu <ubuntu@ip-172-31-42-56.us-east-2.compute.internal>
2020-09-23 20:28:54 +0000

    updates

--- web-stack/server/app/express.js
+++ web-stack/server/app/express.js
@@ -2,14 +2,14 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const apiRouter = require('../api/routes')
 
-const clientDir = `${__dirname}/../../../client/build/`
+const clientDir = `${__dirname}/../../client/build/`
 const clientIndex = clientDir + 'index.html'
 
 const app = express()
 
 app.use(cookieParser())
 
-app.use((req, _, next) => {
+app.use((req, res, next) => {
   next()
 })
 
@@ -18,8 +18,9 @@ app.use('/api', express.json(), apiRouter)
 app.use(express.static(clientDir))
 
 app.get('*', (req, res) => {
-  if (!req.xhr) res.sendFile(clientIndex)
+  if (!req.xhr) res.sendFile('index.html', { root: clientDir })
   else res.sendStatus(404)
+  return
 })
 
 module.exports = app
