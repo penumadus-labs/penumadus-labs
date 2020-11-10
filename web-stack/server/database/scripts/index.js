17926af923f29880513298f911bbe6e30ccc057b
Abe <abramabradley@gmail.com>
2020-11-10 11:57:16 -0500

    saved data from bridge test

---
+++ web-stack/server/database/scripts/index.js
@@ -0,0 +1,4 @@
+const client = require('./client-ssh')
+const { writeFileSync } = require('fs')
+
+client.wrap(async () => {})
--- web-stack/server/database/scripts/save-data.js
+++
@@ -1,7 +0,0 @@
-const client = require('./client')
-
-const id = 'morganbridge'
-
-client.wrap(async () => {
-  const bridgeTest = await client.find({ id })
-})
