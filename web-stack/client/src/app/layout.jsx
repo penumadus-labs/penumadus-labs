a161391063cdcc85d2f7263b6d4a052d06239a9c
Abe <abramabradley@gmail.com>
2020-11-18 18:16:59 -0500

    trying to fix mobile layout bug

--- web-stack/client/src/app/layout.jsx
+++ web-stack/client/src/app/layout.jsx
@@ -9,6 +9,7 @@ import StatusBar from './status-bar'
 const Root = styled.div`
   display: grid;
   height: ${window.innerHeight}px;
+  overflow-y: hidden;
 
   ${({ theme }) => theme.gt.layout} {
     grid-template-rows: auto minmax(0, 1fr);
