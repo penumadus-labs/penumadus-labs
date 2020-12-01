ac603473e8ae1caa3b4771312880f1da90b0f184
Abe <abramabradley@gmail.com>
2020-12-01 05:48:30 -0500

    bug fix

--- web/server/app.js
+++ web/server/app.js
@@ -4,7 +4,9 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const apiRouter = require('./api/routes')
 
-const dev = process.env.NODE_ENV === 'development'
+const { NODE_ENV, REACT_APP_MOUNT_PATH } = process.env
+
+const dev = NODE_ENV === 'development'
 
 const originList = ['http://localhost:3000', 'http://hankthetank.me:3000']
 const corsConfig = {
@@ -24,9 +26,9 @@ module.exports = express()
   .use(test)
   .use(cors(corsConfig))
   .use(cookieParser())
-  .use(`${process.env.REACT_APP_MOUNT_PATH}/api`, express.json(), apiRouter)
+  .use(`${REACT_APP_MOUNT_PATH}/api`, express.json(), apiRouter)
   // .use((_, res, next) => (dev ? res.sendStatus(404) : next()))
-  .use(express.static(clientDir))
+  .use(REACT_APP_MOUNT_PATH, express.static(clientDir))
   .get('*', (req, res) => {
     if (!req.xhr) res.sendFile('index.html', { root: clientDir })
     else return res.sendStatus(404)
