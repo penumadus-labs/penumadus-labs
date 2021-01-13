7b02a52de61a1e5f8154687dad1be96eefcdf3e1
Abe <abramabradley@gmail.com>
2021-01-12 22:46:00 -0500

    building layout

---
+++ public/components/YouTubeVideo.tsx
@@ -0,0 +1,12 @@
+export default function YouTubeVideo() {
+  return (
+    <div className="responsive-iframe-container">
+      <iframe
+        src="https://www.youtube-nocookie.com/embed/pSOBWOKEmJE"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
+  )
+}
